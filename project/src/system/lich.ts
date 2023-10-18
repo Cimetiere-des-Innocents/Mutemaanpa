b26f0f694706c337619dbde443228a6d6d285b84
Chlamydomonos <2457242458@qq.com>
2023-10-19 01:14:22 +0800

    lich v1.1

--- project/src/system/lich.ts
+++ project/src/system/lich.ts
@@ -17,38 +17,46 @@ type EventResult = PassResult | AbortResult;
 
 type LichAction<T> = (owner: T) => Promise<void>;
 
-interface LichEvent<Params, Owner> {
-    params: Params;
-    owner: Owner;
-    addAction: (action: LichAction<Owner>) => void;
+interface LichEvent<P, O> {
+    params: P;
+    owner: O;
+    addAction: (action: LichAction<O>) => void;
     pass: (name?: string) => PassResult;
     stop: () => AbortResult;
     rollback: () => AbortResult;
 }
 
-interface LichGuildEvent<Params, Owner> extends LichEvent<Params, Owner> {
-    actionList: LichAction<Owner>[];
+interface LichGuildEvent<P, O> extends LichEvent<P, O> {
+    actionList: LichAction<O>[];
 }
 
-type LichEventCallback<Params, Owner> = (
-    event: LichEvent<Params, Owner>
-) => EventResult;
+type LichEventCallback<P, O> = (event: LichEvent<P, O>) => EventResult;
 
-class Lich<Params, EventOwner> {
+const IsLich = Symbol('IsLich');
+
+type LichParams<Params, Owner> = {
+    name: string;
+    execute: LichEventCallback<Params, Owner>;
+    [IsLich]?: undefined;
+};
+
+class Lich<P, O> {
     constructor(
         readonly name: string,
-        readonly execute: LichEventCallback<Params, EventOwner>
+        readonly execute: LichEventCallback<P, O>
     ) {}
 
-    previous: Lich<Params, EventOwner> | undefined;
-    following = new Map<string, Lich<Params, EventOwner>>();
-    firstFollowing: Lich<Params, EventOwner> | undefined;
+    [IsLich] = true;
 
-    parent: Lich<Params, EventOwner> | undefined;
-    children = new Map<string, Lich<Params, EventOwner>>();
-    firstChild: Lich<Params, EventOwner> | undefined;
+    previous: Lich<P, O> | undefined;
+    following = new Map<string, Lich<P, O>>();
+    firstFollowing: Lich<P, O> | undefined;
 
-    onEvent(event: LichGuildEvent<Params, EventOwner>): EventResult {
+    parent: Lich<P, O> | undefined;
+    children = new Map<string, Lich<P, O>>();
+    firstChild: Lich<P, O> | undefined;
+
+    onEvent(event: LichGuildEvent<P, O>): EventResult {
         let result = this.firstChild?.onEvent(event) ?? event.pass();
         if (result.type === ResultType.PASS) {
             result = this.execute(event);
@@ -65,57 +73,110 @@ class Lich<Params, EventOwner> {
         }
         return result;
     }
+
+    addFollowing(lich: LichParams<P, O>) {
+        const followingLich = new Lich(lich.name, lich.execute);
+
+        this.following.set(lich.name, followingLich);
+        this.firstFollowing ??= followingLich;
+        followingLich.previous = this;
+        followingLich.parent = this.parent;
+        this.parent?.children.set(lich.name, followingLich);
+
+        return followingLich;
+    }
+
+    addChild(lich: LichParams<P, O>) {
+        const childLich = new Lich(lich.name, lich.execute);
+
+        this.children.set(lich.name, childLich);
+        if (this.firstChild) {
+            this.firstChild.previous = childLich;
+            childLich.following.set(this.firstChild.name, this.firstChild);
+            childLich.firstFollowing ??= this.firstChild;
+        }
+        this.firstChild = childLich;
+        childLich.parent = this;
+
+        return childLich;
+    }
 }
 
-type LichParams<Params, Owner> = {
-    name: string;
-    execute: LichEventCallback<Params, Owner>;
-};
+/**
+ * LichGuild类 - 死灵法师协会
+ *
+ * 死灵法师协会负责管理所有死灵法师，以及把事件传递给死灵大法师(rootLich)。
+ * 死灵法师协会中，死灵大法师首先接收到事件，然后先把事件传给自己的每个子节点，再执行自己的事件处理函数，最后把事件传给自己的后继（如果有的话）。
+ * 创建死灵法师协会时，需要传入死灵大法师。
+ * 注意，和死灵法师协会有关的方法中，传入的死灵法师都不是Lich对象，而是LichParams对象。这防止一个Lich递归调用自己。
+ *
+ * 注意addChild函数的特殊行为：它把新的死灵法师插入到子节点列表的最前面，而不是最后面。
+ * TODO: 在Entity中，定义发送事件的方法，它调用LichGuild的onEvent方法。
+ */
+export class LichGuild<P, O> {
+    private liches = new Map<string, Lich<P, O>>();
+    readonly rootLich: Lich<P, O>;
 
-export class LichGuild<Params, EventOwner> {
-    private liches = new Map<string, Lich<Params, EventOwner>>();
-    readonly rootLich: Lich<Params, EventOwner>;
-
-    constructor(rootLich: LichParams<Params, EventOwner>) {
+    constructor(rootLich: LichParams<P, O>) {
         this.rootLich = new Lich(rootLich.name, rootLich.execute);
         this.liches.set(rootLich.name, this.rootLich);
     }
 
-    addFollowing(lichName: string, following: LichParams<Params, EventOwner>) {
-        const lich = this.liches.get(lichName);
-        if (!lich) {
-            throw new Error(`lich ${lichName} not found`);
+    addFollowing(lichName: string, following: LichParams<P, O>): Lich<P, O>;
+    addFollowing(following: LichParams<P, O>): Lich<P, O>;
+
+    addFollowing(p1: string | LichParams<P, O>, p2?: LichParams<P, O>) {
+        let lich: Lich<P, O>, following: Lich<P, O>;
+        if (typeof p1 === 'string') {
+            lich = this.liches.get(p1)!;
+            if (!lich) {
+                throw new Error(`lich ${p1} not found`);
+            }
+            following = new Lich(p2!.name, p2!.execute);
+        } else {
+            lich = this.rootLich;
+            following = new Lich(p1.name, p1.execute);
         }
 
-        const followingLich = new Lich(following.name, following.execute);
+        lich.following.set(following.name, following);
+        lich.firstFollowing ??= following;
+        following.previous = lich;
+        following.parent = lich.parent;
+        lich.parent?.children.set(following.name, following);
 
-        lich.following.set(following.name, followingLich);
-        lich.firstFollowing ??= followingLich;
-        followingLich.previous = lich;
-        followingLich.parent = lich.parent;
-        lich.parent?.children.set(following.name, followingLich);
+        return following;
     }
 
-    addChild(lichName: string, child: LichParams<Params, EventOwner>) {
-        const lich = this.liches.get(lichName);
-        if (!lich) {
-            throw new Error(`lich ${lichName} not found`);
+    addChild(lichName: string, child: LichParams<P, O>): Lich<P, O>;
+    addChild(child: LichParams<P, O>): Lich<P, O>;
+
+    addChild(p1: string | LichParams<P, O>, p2?: LichParams<P, O>) {
+        let lich: Lich<P, O>, child: Lich<P, O>;
+        if (typeof p1 === 'string') {
+            lich = this.liches.get(p1)!;
+            if (!lich) {
+                throw new Error(`lich ${p1} not found`);
+            }
+            child = new Lich(p2!.name, p2!.execute);
+        } else {
+            lich = this.rootLich;
+            child = new Lich(p1.name, p1.execute);
         }
 
-        const childLich = new Lich(child.name, child.execute);
-
-        lich.children.set(child.name, childLich);
+        lich.children.set(child.name, child);
         if (lich.firstChild) {
-            lich.firstChild.previous = childLich;
-            childLich.following.set(lich.firstChild.name, lich.firstChild);
-            childLich.firstFollowing ??= lich.firstChild;
+            lich.firstChild.previous = child;
+            child.following.set(lich.firstChild.name, lich.firstChild);
+            child.firstFollowing ??= lich.firstChild;
         }
-        lich.firstChild = childLich;
-        childLich.parent = lich;
+        lich.firstChild = child;
+        child.parent = lich;
+
+        return child;
     }
 
-    async onEvent(params: Params, eventOwner: EventOwner) {
-        const actionList: LichAction<EventOwner>[] = [];
+    async onEvent(params: P, eventOwner: O) {
+        const actionList: LichAction<O>[] = [];
         const result = this.rootLich.onEvent({
             params,
             owner: eventOwner,
