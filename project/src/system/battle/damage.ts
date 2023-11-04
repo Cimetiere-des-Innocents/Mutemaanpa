8c65d67d39bee35d8914dbe26a24af8e61fa474c
Chlamydomonos <2457242458@qq.com>
2023-11-04 21:02:31 +0800

    有关剧情线的草稿

--- project/src/system/battle/damage.ts
+++ project/src/system/battle/damage.ts
@@ -2,4 +2,4 @@ import { Health } from '../components/health';
 import type { GameEvent } from '../event';
 import Option from '../utils/option';
 
-const canDamage = (health: Health, )
+//const canDamage = (health: Health, )
